86afc5836c36bacb9a8b166225c47f555dc38f62
MattyChance <chenmatty.wang@gmail.com>
2016-10-25 00:02:14 +0000

    added exercise 4

--- index.js
+++ index.js
@@ -1,22 +1,32 @@
 var express = require('express');
+var reddit = require('./reddit');
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'mattychance', // CHANGE THIS :)
+  password : '',
+  database: 'reddit'
+});
+var redditAPI = reddit(connection);
+
 
 var myApp = express();
 
 myApp.get('/hello', function(request, response) {
     console.log(request.query);
     response.send('<h1>Hello, ' + request.query.name + '!</h1>');
-}) 
+}); 
 
 
 myApp.get('/calculator/:op' ,function(request, response) {
-    console.log(request.query); 
-    console.log(request.params);
+    // console.log(request.query); 
+    // console.log(request.params);
     var num1 = parseInt(request.query.num1);
     var num2 = parseInt(request.query.num2);
     var solution;
     switch (request.params.op) {
         case 'add':
-            // code
             solution = num1 + num2;
             break;
         case 'sub':
@@ -34,10 +44,48 @@ myApp.get('/calculator/:op' ,function(request, response) {
     }
     
     response.send({"operator": request.params.op, "firstOperand": request.query.num1, "secondOperand": request.query.num2, "solution": solution});
-})
+});
+
+
+myApp.get('/posts', function(request, response) {
+
+    redditAPI.getAllPosts({
+        numPerPage: 5,
+        page: 0,
+        sortingMethod: 'createdAt'
+    }, function(err, result) {
+        if (err) {
+            console.log(err, 'Something went wrong when retrieving the psots. Try again later.');
+        }
+        else {
+            var html = [`<div id="contents">
+                            <h1>Five Newest Posts</h1>
+                                <ul class="contents-list">`];
+            for (var i = 0; i < result.length; i++) {
+        
+                  html.push(`<li class="content-item">
+                                  <h2 class="content-item__title">
+                                    <a href=${result[i].postURL}>${result[i].title}</a>
+                                  </h2>
+                                  <p>Created by ${result[i].user.username}</p>
+                                </li>`);
+                  html.push(` </ul>
+                            </div>`);
+                 
+            };
+
+            response.send(html.join(''));
+        }
+        connection.end();
+    });
+
+});
 
 var server = myApp.listen(process.env.PORT, process.env.IP, function(){
     var host = server.address().address;
     var port = server.address().port;
     console.log('myApp listening at http://%s:%s', host, port );
 });
+
+
+    
